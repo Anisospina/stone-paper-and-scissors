7d7d0565b52578dd831ca7b4cf80c378f4cdf900
anaospinarojas <anisospina@gmail.com>
2018-12-05 09:40:55 -0500

    First screen

---
+++ client/src/app/screens/Game/index.js
@@ -0,0 +1,16 @@
+import React, { component, Fragment } from 'react';
+import style from './style';
+
+class Game extends component {
+
+render () {
+return(
+  <Fragment>
+    <h2 className='style.titulo'>Stone Paper and Scissors</h2>
+  </Fragment>
+)
+}
+
+}
+
+export default Game;
