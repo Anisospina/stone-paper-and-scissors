97207a4dbefa7149e4d74f415a3fb353a1c51dea
anaospinarojas <anisospina@gmail.com>
2018-12-06 14:13:31 -0500

    Create card

--- client/src/app/screens/Game/index.js
+++ client/src/app/screens/Game/index.js
@@ -2,12 +2,23 @@ import React, { Component, Fragment } from 'react';
 import './style.scss';
 
 class Game extends Component {
+
   
+  
+handleStone = () => {
+
+}
+
   render () {
     return(
       <Fragment>
       <h2 className="title">Stone Paper and Scissors</h2>
-      <div className="probando">probando</div>
+      
+      <div className="cards">
+      <button className="stone" onclick={this.handleStone} >Piedra</button>
+      <button className="paper" onclick={this.handlePaper} >Paper</button>
+      <button className="cut" onclick={this.handlecut} >Cut</button>
+      </div>
       </Fragment>
       )
     }
